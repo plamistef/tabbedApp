e2dff9df8efb38663e525013af3e5d5cf0df64bd
plamistef <plamstef@icloud.com>
2020-06-18 15:45:22 +0200

    Initial Commit

---
+++ tabbedApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tabbedApp
+//
+//  Created by Plam Stefanova on 6/18/20.
+//  Copyright © 2020 Plam Stefanova. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tabbedApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  tabbedApp
+//
+//  Created by Plam Stefanova on 6/18/20.
+//  Copyright © 2020 Plam Stefanova. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
